d6f8599ab4088685cae76b41a7f3b864a63daca5
weeradab <66011292@kmitl.ac.th>
2025-06-23 19:04:27 +0000

    complete

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+while True:
+    put_input = input("What you gotta say? : ")
+    print("I got that! Anything else? :", put_input)
+    if put_input == "STOP":
+        break
