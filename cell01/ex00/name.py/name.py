ad92bb0c918d9904bf61800283eac2a58b46e1fb
weeradab <66011292@kmitl.ac.th>
2025-06-23 08:56:56 +0000

    complete

---
+++ cell01/ex00/name.py/name.py
@@ -0,0 +1,3 @@
+first_name = "Weerada"
+last_name = "Bootdeeart"
+print(first_name,last_name)
---
+++ cell01/ex01/name.py/name.py
@@ -0,0 +1,4 @@
+first_name = "Weerada"
+last_name = "Bootdeeart"
+whole_name = first_name + " " + last_name
+print(whole_name)
