82caa633efa3aa479a0e4ed9a718a361495cbd51
weeradab <66011292@kmitl.ac.th>
2025-06-24 10:51:40 +0000

    complete

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#import the math module
+import math
+num = float(input("give me a number: "))
+print(math.ceil(num))   #method rounds a number rounded UP to the nearest integer
