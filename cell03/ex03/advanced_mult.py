fe702be24eba1b379753ef84f054ccb187c15993
weeradab <66011292@kmitl.ac.th>
2025-06-24 08:31:43 +0000

    complete

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -3,11 +3,11 @@ i = 0  #track the current table (0 to 10)
 
 while i <= 10:
     print(f"Table de {i}:", end=" ")
-    
-    j = 0  #track the multiplier (0 to 10)
-    while j <= 10:
-        print(i * j, end=" ")
-        j += 1
+        
+            j = 0  #track the multiplier (0 to 10)
+                while j <= 10:
+                        print(i * j, end=" ")
+                                j += 1
 
-    print()
-    i += 1
+                                    print()
+                                        i += 1
---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#ask for the word 
+word = input("give me a word: ")
+# Convert the word to uppercase and print it
+print(word.upper())
