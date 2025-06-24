6d9ad60876b7e45f2dddda2e1a60bcaf6c67a963
weeradab <66011292@kmitl.ac.th>
2025-06-24 15:16:20 +0000

    complete

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys  #which provides access to command-line arguments
+
+# Count of arguments passed (excluding the script name)
+para_count = len(sys.argv) - 1
+print(f"Number of parameters: {para_count}")
