a53c32762f564336e6aeedb8a47b4d778591d8ed
weeradab <66011292@kmitl.ac.th>
2025-06-24 09:26:14 +0000

    complete

---
+++ cell04/ex02/: calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+fnum = int(input("give me the first number: " ))
+snum = int(input("give me the second number: "))
+print("thank you")
+print(f"{fnum} + {snum} = {fnum + snum}" )
+print(f"{fnum} - {snum} = {fnum - snum}" )
+print(f"{fnum} / {snum} = {fnum // snum}")  
+print(f"{fnum} * {snum} = {fnum * snum}" )
