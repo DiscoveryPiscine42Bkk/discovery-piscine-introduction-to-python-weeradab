19cabb36915863bebc948316f89cb769a7957f9a
weeradab <66011292@kmitl.ac.th>
2025-06-24 10:29:54 +0000

    complete

---
+++ cell04/ex03/float.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+num = float(input("give me a number: "))
+if num == int(num):  # Checks if the number equals its integer 
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
