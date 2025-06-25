f5510fd5ba926e8d59dfecdc1a2a7685e9170fb9
weeradab <66011292@kmitl.ac.th>
2025-06-25 13:27:29 +0000

    complete

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+def main():
+    
+    arguments = sys.argv[1:]
+    
+    if len(arguments) != 1:
+        print("none")
+        return
+    
+    input_string = arguments[0]
+    count = input_string.count('z')
+    
+    # Handle the z count cases
+    if count == 0:
+        print("none")
+    else:
+        print('z' * count)
+
+if __name__ == "__main__":
+    main()
